b851e99ace9563bd3cadd9dc8bfafee9a32b78c3
Mhmdraflyy <raflimuhammad646@gmail.com>
2026-03-31 11:00:33 +0700

    fix: add ScrollToTop on route change

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import CompetitionSystem from "./pages/CompetitionSystem";
 import Partnership from "./pages/Partnership";
 import Membership from "./pages/Membership";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
